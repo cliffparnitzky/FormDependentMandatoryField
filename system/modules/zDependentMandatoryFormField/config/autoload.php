1c24d2dfce912a1ac04f21c0688232c831d51c3f
Cliff Parnitzky <github@cliff-parnitzky.de>
2012-11-08 18:48:39 +0100

    Fixed issue #6 and added autoloader files

---
+++ system/modules/zDependentMandatoryFormField/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package ZDependentMandatoryFormField
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'DependentMandatoryFormField' => 'system/modules/zDependentMandatoryFormField/DependentMandatoryFormField.php',
+));
